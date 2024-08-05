dab41416a272cd0c3c277f5a862ebc923d7c2264
Cauan Leite <cauanfc@hotmail.com>
2024-08-05 08:56:32 -0300

    First commit

---
+++ src/main/java/com/cauan/estudos/Spring/Expert/SpringExpertApplication.java
@@ -0,0 +1,13 @@
+package com.cauan.estudos.Spring.Expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cauan/estudos/Spring/Expert/SpringExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauan.estudos.Spring.Expert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
