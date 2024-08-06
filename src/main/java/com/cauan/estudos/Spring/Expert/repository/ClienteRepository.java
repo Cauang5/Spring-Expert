b0b1c0256d99f6c488796eae3e6b071fdd87bcb7
Cauan Leite <cauanfc@hotmail.com>
2024-08-06 11:56:12 -0300

    feat: Atualização da classe cliente e cliente repository para testes

--- src/main/java/com/cauan/estudos/Spring/Expert/repository/ClienteRepository.java
+++ src/main/java/com/cauan/estudos/Spring/Expert/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.cauan.estudos.Spring.Expert.repository;
 
 import com.cauan.estudos.Spring.Expert.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     List<Cliente> findByNomeLike(String nome);
 
     boolean existsByNome(String nome);
+
+    @Query("select c from Cliente c left join fetch c.pedidos where c.id = :id")
+    Cliente findClienteFetchPedidos(@Param("id") Long id);
 }
